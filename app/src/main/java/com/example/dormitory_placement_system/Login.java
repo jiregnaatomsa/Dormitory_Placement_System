983e8ca82aff9820e4e77dcd423a9209255bdb44
jiregnaatomsa <149964953+jiregnaatomsa@users.noreply.github.com>
2024-01-30 20:44:02 +0300

    Update Login.java

--- app/src/main/java/com/example/dormitory_placement_system/Login.java
+++ app/src/main/java/com/example/dormitory_placement_system/Login.java
@@ -6,7 +6,6 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
